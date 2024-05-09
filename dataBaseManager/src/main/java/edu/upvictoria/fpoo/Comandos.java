1a8b0149135814cdf0400185a33ce64ced04485e
Miguel Angel <2230309@upv.edu.mx>
2024-05-08 21:25:26 -0600

    creacion de comandos

--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/Comandos.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/Comandos.java
@@ -1,4 +1,17 @@
 package edu.upvictoria.fpoo;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Comandos {
+    private static Map<String, Runnable> comando = new HashMap<String, Runnable>();
+comando.put("show TBALES", this::showTables);
+comando.put("DROP TABLE", () -> {
+        try{
+
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    });
+
 }
